ed4abfd7ab96e8e12f0e867913f63aeb0929747b
Isael silva <isaelsilva1039@gmail.com>
2025-04-21 23:29:29 -0300

    Ajuste de logs

--- src/main/java/java_core_api/api_java_core/controller/ColetorNfController.java
+++ src/main/java/java_core_api/api_java_core/controller/ColetorNfController.java
@@ -61,7 +61,7 @@ public class ColetorNfController {
 
     @GetMapping("/listar/pendente")
     public ResponseEntity<?> listarNotasPendete() {
-        log.info("🔍 Iniciando listagem de notas fiscais pendentes [][] ✅ {},");
+        log.info("🔍 Iniciando listagem de notas fiscais pendentes,");
 
         List<NotaFiscal> notas = notaFiscalService.buscarNotasPendentes();
 
